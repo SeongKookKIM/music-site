27a7040c71ff57304a36203ef545faa2fe43d688
SeongkookKIM <ksk8646@naver.com>
2023-07-11 12:08:45 +0900

    날씨 api 및 리덕스 비디오

--- music/src/Store.js
+++ music/src/Store.js
@@ -9,8 +9,41 @@ let backgroundVideo = createSlice({
   ],
 });
 
+let backgroundRainVideo = createSlice({
+  name: "backgroundRainVideo",
+  initialState: ["assets/video/비.mp4", "assets/video/비2.mp4"],
+});
+
+let backgroundTravelVideo = createSlice({
+  name: "backgroundTravelVideo",
+  initialState: [
+    "assets/video/travel-day.mp4",
+    "assets/video/travel-night.mp4",
+  ],
+});
+
+let backgroundDriveVideo = createSlice({
+  name: "backgroundDriveVideo",
+  initialState: ["assets/video/드라이브.mp4"],
+});
+
+let backgroundSadVideo = createSlice({
+  name: "backgroundSadVideo",
+  initialState: ["assets/video/슬픈음악.mp4", "assets/video/슬픈음악2.mp4"],
+});
+
+let backgroundStudyVideo = createSlice({
+  name: "backgroundStudyVideo",
+  initialState: ["assets/video/집중.mp4", "assets/video/집중2.mp4"],
+});
+
 export default configureStore({
   reducer: {
     backgroundVideo: backgroundVideo.reducer,
+    backgroundRainVideo: backgroundRainVideo.reducer,
+    backgroundTravelVideo: backgroundTravelVideo.reducer,
+    backgroundDriveVideo: backgroundDriveVideo.reducer,
+    backgroundSadVideo: backgroundSadVideo.reducer,
+    backgroundStudyVideo: backgroundStudyVideo.reducer,
   },
 });
--- music/src/components/Background.js
+++ music/src/components/Background.js
@@ -5,9 +5,48 @@ function Background() {
   const [timeBg, setTimeBg] = useState("");
   const [backTimeHours, setBackTimeHours] = useState(new Date().getHours());
   const [backTimeSec, setBackTimeSec] = useState(new Date().getSeconds());
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [weather, setWeather] = useState({});
+
+  // 날씨
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      setLatitude(lat);
+      setLongitude(lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=904ccd662ecc191d3296c2a1bbd98138&units=metric`;
+    let res = await fetch(url);
+    let data = await res.json();
+    setWeather(data);
+    // console.log(weather.weather[0].main);
+    // console.log(weather);
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      setTimeout(() => {
+        getWeatherByCurrentLocation(latitude, longitude);
+      }, 100);
+    }
+  }, [latitude]);
 
   // Time
   let bgList = useSelector((state) => state.backgroundVideo);
+  let bgRainList = useSelector((state) => state.backgroundRainVideo);
+  let bgTravelList = useSelector((state) => state.backgroundTravelVideo);
+  let bgDriveList = useSelector((state) => state.backgroundDriveVideo);
+  let bgSadList = useSelector((state) => state.backgroundSadVideo);
+  let bgStudyList = useSelector((state) => state.backgroundStudyVideo);
 
   useEffect(() => {
     let bgTimer = setInterval(() => {
